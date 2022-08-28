edf7a1d52e678f97c1c6109e1e3f52c3a0298dcb
Nick Lawler <lawler145@gmail.com>
2022-08-28 19:52:03 -0400

    hello site

---
+++ routes/about.tsx
@@ -0,0 +1,11 @@
+/** @jsx h */
+import { h } from "preact";
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is about page</p>
+    </main>
+  );
+}
