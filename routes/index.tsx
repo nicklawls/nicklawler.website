a532a19b2a65cc62c533e1045cacc744b0bd4dfd
Nick Lawler <nick@nicklawler.website>
2023-02-23 01:51:05 -0500

    Uprade to fresh 1.1 w/codemod

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,17 +1,14 @@
-/** @jsx h */
-import { h } from "preact";
-import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
-    <div class={tw`p-4 mx-auto max-w-screen-md`}>
+    <div class="p-4 mx-auto max-w-screen-md">
       <img
         src="/logo.svg"
         height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
-      <p class={tw`my-6`}>
+      <p class="my-6">
         Hi Ian.
       </p>
     </div>
