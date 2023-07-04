875be41501aa76e458c2138181f70f2428a24627
Nick Lawler <nick@nicklawler.website>
2023-07-04 13:28:25 -0400

    all mono

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,7 +1,7 @@
 const Header = () => (
   <div class="mx-auto w-[600px]">
     <div class="flex flex-col space-y-3">
-      <h1 class="font-bold text-3xl font-mono">
+      <h1 class="font-bold text-3xl">
         Nick Lawler Web Site
       </h1>
       <div class="flex flex-row space-x-10 text-xl">
@@ -17,7 +17,9 @@ export default function Home() {
   return (
     <body class="bg-peachyellow text-indigodye
                  dark:bg-indigodye dark:text-peachyellow
-                 w-screen h-screen py-10">
+                 font-mono
+                 w-screen h-screen
+                 py-10">
       <Header/>
     </body>
   );
