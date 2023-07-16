9587ab0e5df087243704267cfe6bcb4efb7080c6
Nick Lawler <nick@nicklawler.website>
2023-07-16 16:07:23 -0400

    faqy

--- routes/faq.tsx
+++ routes/faq.tsx
@@ -1,6 +1,17 @@
 import { Head } from "$fresh/runtime.ts";
+import { ComponentChildren } from "preact";
 import { Header, PAGES } from "../components/Header.tsx";
 import { SITE_TITLE } from "../title.ts";
+
+const Q = ({ children }: { children: ComponentChildren }) => (
+  <i>
+    <strong>{children}</strong>
+  </i>
+);
+
+const A = ({ children }: { children: ComponentChildren }) => <p>{children}</p>;
+
+const link = { class: "underline", target: "_blank" };
 
 export default function FaqPage() {
   return (
@@ -9,9 +20,38 @@ export default function FaqPage() {
         <title>{`F.A.Q - ${SITE_TITLE}`}</title>
       </Head>
       <Header selectedPathname={PAGES.faq.pathname} title={SITE_TITLE} />
-      <main>
-        <h1>About</h1>
-        <p>This is about page</p>
+      <main class="space-y-10">
+        <div>
+          <Q>What is this site?</Q>
+          <A>The software development weblog of Nick Lawler.</A>
+        </div>
+        <div>
+          <Q>Who is Nick Lawler?</Q>
+          <A>
+            Software Engineer on the{" "}
+            <a
+              {...link}
+              href="https://www.extrahop.com"
+            >
+              ExtraHop
+            </a>{"  "}
+            UI team.
+          </A>
+        </div>
+        <div>
+          <Q>What's the best way to get a hold of Nick?</Q>
+          <A>
+            Email <i>nick</i> "at" this site's domain, or comment on the
+            <br/>
+            <a
+              {...link}
+              href="https://github.com/nicklawls/nicklawler.website"
+            >
+              GitHub Repo
+            </a>
+            .
+          </A>
+        </div>
       </main>
     </>
   );
