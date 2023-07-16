d3d01f7e75427023d5642583c3a798f274f75aff
Nick Lawler <nick@nicklawler.website>
2023-07-16 17:45:30 -0400

    mobile ftw

--- main.ts
+++ main.ts
@@ -10,4 +10,7 @@ import manifest from "./fresh.gen.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, {
+  plugins: [twindPlugin(twindConfig)],
+  hostname: "localhost",
+});
--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,13 +1,26 @@
 import { AppProps } from "$fresh/server.ts";
+import { css } from "twind/css";
+import { tw } from "twind";
+
+const globalStyles = css({
+  ":global": {
+    body: {
+      "@apply": [
+        "bg-peachyellow",
+        "dark:bg-indigodye",
+        "text-indigodye",
+        "dark:text-peachyellow",
+      ],
+    },
+  },
+});
 
 export default function App({ Component }: AppProps) {
   return (
-    <div class="bg-peachyellow text-indigodye
-                dark:bg-indigodye dark:text-peachyellow
-                font-mono
-                h-screen
-                py-10">
-      <div class="mx-auto w-[600px] flex flex-col space-y-10">
+    <div class={`${tw(globalStyles)} font-mono h-screen py-10`}>
+      <div class="px-5 w-200 
+                  md:mx-auto md:w-[600px] md:p-0
+                  flex flex-col space-y-10">
         <Component />
       </div>
     </div>
