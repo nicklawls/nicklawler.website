66fd36f99cd122d4fbc89bba2c952d50ad44ec5d
Nick Lawler <nick@nicklawler.website>
2023-07-16 21:27:19 -0400

    emoji favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "$fresh/server.ts";
+import { Head } from "$fresh/runtime.ts";
 import { css } from "twind/css";
 import { tw } from "twind";
 
@@ -17,12 +18,21 @@ const globalStyles = css({
 
 export default function App({ Component }: AppProps) {
   return (
-    <div class={`${tw(globalStyles)} font-mono h-screen py-10`}>
-      <div class="px-5 w-200 
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥁</text></svg>"
+        >
+        </link>
+      </Head>
+      <div class={`${tw(globalStyles)} font-mono h-screen py-10`}>
+        <div class="px-5 w-200 
                   md:mx-auto md:w-[600px] md:p-0
                   flex flex-col space-y-10">
-        <Component />
+          <Component />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
