3799d9d9ca5b6598bb3614574223261cc814bf1b
Nick Lawler <nick@nicklawler.website>
2025-02-27 01:16:11 -0500

    b-unification / birdification comes for the dev log

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import { $, serve } from "bun";
+import { $, file, serve } from "bun";
 import { type ReactNode } from "react";
 import { renderToStaticMarkup } from "react-dom/server";
 
@@ -113,11 +113,14 @@ function FaqPage() {
 const PATH_CSS_OUT = "/index.css" as const;
 
 /** Has to be a string that react renders into, to control the <head> and <title> */
-function appTemplate(body: string): string {
+function appTemplate(
+  body: ReactNode,
+  head?: ReactNode,
+): string {
   return `<!DOCTYPE html>
      <html lang="en">
       <head>
-        <title>${SITE_TITLE}</title>
+        ${renderToStaticMarkup(head ?? <title>{SITE_TITLE}</title>)}
         <link
           rel="icon"
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥁</text></svg>"
@@ -128,10 +131,69 @@ function appTemplate(body: string): string {
         <div
           class="font-mono px-5 w-200 md:mx-auto md:w-[600px] md:p-0 flex flex-col space-y-10"
         >
-          ${body}
+          ${renderToStaticMarkup(body)}
         </div>
       </body>
     </html>`;
+}
+
+/** Type predicate infrence means that only the ones with show: true make it! */
+export const ENTRIES = (
+  [
+    {
+      slug: "hello-world",
+      title: "Hello World",
+      date: new Date("7-4-2023"),
+      file: "hello-world.md",
+      show: false,
+    },
+    {
+      slug: "hello-world-2",
+      title: "Hello World 2",
+      date: new Date("7-6-2023"),
+      file: "hello-world-2.md",
+      show: true,
+    },
+    {
+      slug: "metapost",
+      title: "How to Build This Website",
+      date: new Date("7-21-2024"),
+      file: "metapost.md",
+      show: true,
+    },
+  ] as const
+)
+  .filter((entry) => entry.show)
+  .sort((a, b) => a.date.getTime() - b.date.getTime());
+
+function EntryPage({
+  entry,
+  content,
+}: {
+  entry: (typeof ENTRIES)[number];
+  content: string;
+}) {
+  return (
+    <div className="flex flex-col space-y-3">
+      <Header title={entry.title} />
+      <div>
+        <time className="text-gray-500">
+          {entry.date.toLocaleDateString("en-us", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div
+          data-color-mode="auto"
+          data-light-theme="light"
+          data-dark-theme="dark"
+        >
+          <div className="mt-8 markdown-body">{content}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 serve({
@@ -139,7 +201,7 @@ serve({
   port: process.env["PORT"] ?? 3000,
   development: process.env.NODE_ENV !== "production",
   routes: {
-    "/": new Response(appTemplate(renderToStaticMarkup(<FaqPage />)), {
+    "/": new Response(appTemplate(<FaqPage />), {
       headers: { "Content-Type": "text/html" },
     }),
     [PATH_CSS_OUT]: new Response(
@@ -153,13 +215,28 @@ serve({
     ),
   },
   fetch: async (request, _server) => {
-    return new Response(
-      appTemplate(
-        renderToStaticMarkup(
-          <p>404 not found: {new URL(request.url).pathname}</p>,
-        ),
-      ),
-      { headers: { "Content-Type": "text/html" }, status: 404 },
-    );
+    const pathname = new URL(request.url).pathname;
+    const slug = pathname.replace("/", "");
+    // TODO: pre-bake ENTRIES into an object for `routes`
+    const entry = ENTRIES.find((e) => e.slug === slug);
+    if (entry) {
+      try {
+        const content = await file(`./entries/${entry.file}`).text();
+        return new Response(
+          appTemplate(
+            <EntryPage entry={entry} content={content} />,
+            <title>{entry.title}</title>,
+          ),
+          {
+            headers: { "Content-Type": "text/html" },
+          },
+        );
+      } catch (_) {}
+    }
+
+    return new Response(appTemplate(<p>404 not found: {pathname}</p>), {
+      headers: { "Content-Type": "text/html" },
+      status: 404,
+    });
   },
 });
